d568fcd7ab30f64d87d948e3a34bff74bf43b0f4
JULIO CESAR SANTANA ALVES <25132583@CFP127ALUNO.LOCAL>
2025-08-25 16:57:04 -0300

    Interface da Calculadora Completa

---
+++ app.js
@@ -0,0 +1,9 @@
+'use stratic'
+const botaoAdicao = document.getElementById('botaoSomar')
+const botaoSubtracao = document.getElementById('botaoSubtrair')
+const botaoMultiplicacao = document.getElementById('botaoMultiplicar')
+const botaoDivisao = document.getElementById('botaoDividir')
+
+const valor1 = document.getElementById('n1')
+const valor2 = document.getElementById('n2')
+const areaResultado = documet.getElementById('resultado')
