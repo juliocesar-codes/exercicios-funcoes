b503a9b84822e000acef0bd047252764cf817538
JULIO CESAR SANTANA ALVES <25132583@CFP127ALUNO.LOCAL>
2025-09-01 15:01:50 -0300

    Atividade 2 e 3 concluída

--- funcoes.js
+++ funcoes.js
@@ -43,25 +43,60 @@ function calcularTotal(){
     let quantia = parseInt(document.getElementById('quantia').value)
     let preco = parseInt(document.getElementById('preco').value)
 
+
     let total = quantia * preco
+
+    if(quantia = ''){
+        
+    }
 
     if(quantia <= 5){
         let valorDesconto = total * 0.02
         let totalDesconto = total - valorDesconto
-        console.log(totalDesconto)
+        document.getElementById('resultadoComDesconto').textContent = `Total (com desconto): R$${totalDesconto}`
+        document.getElementById('resultadoSemDesconto').textContent = `Total (com desconto): R$${total}`
+        document.getElementById('desconto').textContent = `Desconto: R$${valorDesconto}`
      }
     
-     if(quantia > 5 <= 10){
+     else if(quantia > 5 && quantia <= 10){
         let valorDesconto = total * 0.03
         let totalDesconto = total - valorDesconto
-        console.log(totalDesconto)
+        document.getElementById('resultadoComDesconto').textContent = `Total (com desconto): R$${totalDesconto}`
+        document.getElementById('resultadoSemDesconto').textContent = `Total (com desconto): R$${total}`
+        document.getElementById('desconto').textContent = `Desconto: R$${valorDesconto}`
      }
 
-     if(quantia > 10){
+     else if(quantia > 10){
         let valorDesconto = total * 0.05
         let totalDesconto = total - valorDesconto
-        console.log(totalDesconto)
-     }
+        document.getElementById('resultadoComDesconto').textContent = `Total (com desconto): R$${totalDesconto}`
+        document.getElementById('resultadoSemDesconto').textContent = `Total (com desconto): R$${total}`
+        document.getElementById('desconto').textContent = `Desconto: R$${valorDesconto}`
+     }else{} 
+}
 
-     document.getElementById('mensagem').textContent = `TOTAL A PAGAR (SEM DESCONTO) ${total} TOTAL A PAGAR (COM DESCONTO) ${totalDesconto}`
+// ATIVIDADE 3
+
+function calcularAulas(){
+    let quantia = parseInt(document.getElementById('quantiaAulas').value)
+    const nivel = document.getElementById('selecao')
+    const valorSelecionado = nivel.value;
+    let nivel1 = parseInt(document.getElementById('nivel1').value)
+    let nivel2 = parseInt(document.getElementById('nivel2').value)
+    let nivel3 = parseInt(document.getElementById('nivel3').value)
+
+    if(valorSelecionado == nivel1){
+        let totalSalario = quantia * nivel1
+        document.getElementById('salario').textContent = `Salário: R$${totalSalario}`
+    }
+    else if(valorSelecionado == nivel2){
+        let totalSalario = quantia * nivel2
+        document.getElementById('salario').textContent = `Salário: R$${totalSalario}`
+    }
+    else if(valorSelecionado == nivel3){
+        let totalSalario = quantia * nivel3
+        document.getElementById('salario').textContent = `Salário: R$${totalSalario}`
+    }else{}
+
+    
 }
